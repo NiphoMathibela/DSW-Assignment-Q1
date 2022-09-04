086a83d45b10b80754e57f8fa50226460afad9c7
NiphoMathibela <94120861+NiphoMathibela@users.noreply.github.com>
2022-09-04 16:57:32 +0200

    2 files modified

--- src/assets/Card.jsx
+++ src/assets/Card.jsx
@@ -4,7 +4,11 @@ import "./Card.css";
 const Card = () => {
   return(
     <div className= "card">
-      <img src= "https://e7.pngegg.com/pngimages/840/663/png-clipart-brand-logo-visa%E3%81%AE%E3%83%88%E3%83%83%E3%83%97%E3%83%96%E3%83%A9%E3%83%B3%E3%83%89%E6%88%A6%E7%95%A5-product-trademark-visa-gemstone-blue.png" />
+      <div>
+        <img src= "https://github.com/NiphoMathibela/DSW-Assignment-Q1/blob/main/src/assets/visa.png?raw=true" className= "visa" />
+        <img src="https://github.com/NiphoMathibela/DSW-Assignment-Q1/blob/main/src/assets/chip.png?raw=true" className= "chip"/>
+      </div>
+      
     </div>
   )
 }
